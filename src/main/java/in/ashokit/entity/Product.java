1716e8a8a76bb73445a2a71b4716aa885ed9dcf2
Ashok IT <30829130+ashokitschool@users.noreply.github.com>
2022-11-02 17:42:52 +0530

    Update Product.java

--- src/main/java/in/ashokit/entity/Product.java
+++ src/main/java/in/ashokit/entity/Product.java
@@ -5,7 +5,6 @@ import javax.persistence.Id;
 
 import lombok.Data;
 
-@Data
 @Entity
 public class Product {
 
@@ -13,5 +12,29 @@ public class Product {
 	private Integer pid;
 	private String pname;
 	private Double price;
+	
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 
 }
